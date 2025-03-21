a0eb64fca657a63e9d1afbe4a29a41f4b61b0cec
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-03-21 14:38:29 -0300

    feat: retorna 400 se a senha for diferente da confirmacao da senha

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -14,12 +14,16 @@ export default class SignUpController implements Controller {
 
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ["name", "email", "password"];
+      const requiredFields = ["name", "email", "password", "passwordConfirmation"];
 
       for (let field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
+      }
+
+      if(httpRequest.body.password != httpRequest.body.passwordConfirmation){
+        return badRequest(new InvalidParamError("passwordConfirmation"))
       }
 
       const isValid = this.emailValidator.isValid("any_email@mail.com");
