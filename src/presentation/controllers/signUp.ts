04d0f7c6ec4a1e95b9d896c939f9d47c937d358e
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-03-21 11:19:04 -0300

    feat: garante o retorno 400 se o email fornecido for invalido

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,16 +1,29 @@
+import InvalidParamError from "../erros/invalid-param-errors";
 import MissingParamError from "../erros/missing-param-errors";
 import { badRequest } from "../helper/httpHelper";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/emailValidator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
-export default class SignUpController implements Controller{
-  handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password"]
+export default class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor(emailValidator: EmailValidator){
+    this.emailValidator = emailValidator
+  }
 
-    for(let field of requiredFields){
-      if(!httpRequest.body[field]){
-        return badRequest(new MissingParamError(field))
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ["name", "email", "password"];
+
+    for (let field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    
+    if(!isValid){
+      return badRequest(new InvalidParamError('email'))
     }
     return;
   }
