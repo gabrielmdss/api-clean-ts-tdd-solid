eaff9717c11a84f9bf844c8fc7fbb1d04e901bdb
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-03-21 12:10:46 -0300

    refactor: move a criacao do emailValidator para uma metodo de fabrica

--- src/presentation/controllers/signUpController.spec.ts
+++ src/presentation/controllers/signUpController.spec.ts
@@ -1,25 +1,35 @@
-
 import { InvalidParamError, MissingParamError, ServerError } from "../erros";
-import { EmailValidator } from "../protocols/emailValidator";
+import { EmailValidator } from "../protocols";
+
 import SignUpController from "./signUp";
 
 interface SutTypes {
   sut: SignUpController;
   emailValidatorStub: EmailValidator;
 }
-
 const makeSut = (): SutTypes => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      return true;
-    }
-  }
-  const emailValidatorStub = new EmailValidatorStub();
+  const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
   return {
     sut,
     emailValidatorStub,
   };
+};
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+const makeEmailValidatorWithError = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      throw new Error();
+    }
+  }
+  return new EmailValidatorStub();
 };
 
 describe("SignUp Controller", () => {
@@ -92,15 +102,10 @@ describe("SignUp Controller", () => {
     expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
   test("Should return 500 if emailValidator throw", () => {
-    class EmailValidatorStub implements EmailValidator {
-      isValid(email: string): boolean {
-        throw new Error();
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub();
+    const emailValidatorStub = makeEmailValidatorWithError();
     const sut = new SignUpController(emailValidatorStub);
-    jest.spyOn(emailValidatorStub, "isValid").mockImplementationOnce(()=> {
-      throw new ServerError()
+    jest.spyOn(emailValidatorStub, "isValid").mockImplementationOnce(() => {
+      throw new ServerError();
     });
     const httpRequest = {
       body: {
