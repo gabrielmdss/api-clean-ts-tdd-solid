53609c042efd416a3bf41e0061139b79fa003af3
GabrielLinux <gersongabriel_@hotmail.com>
2025-03-27 22:29:43 -0300

    feat: returne 200 if valid data is provider

--- src/presentation/controllers/signUp/signUp.ts
+++ src/presentation/controllers/signUp/signUp.ts
@@ -37,12 +37,15 @@ export default class SignUpController implements Controller {
         return badRequest(new InvalidParamError("email"));
       }
     
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
-      return;
+      return {
+        statusCode: 200,
+        body: account
+      };
     } catch (error) {
       return serverError();
     }
